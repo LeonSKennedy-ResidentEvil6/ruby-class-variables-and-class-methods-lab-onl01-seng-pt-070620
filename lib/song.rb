9bc6f2d2bbe2f28994698848ffceca4e4f322754
Ebo Lee <yiboli.coding@gmail.com>
2020-07-25 05:15:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,7 +55,7 @@ class Song
   def self.artist_count
     artist_count = {}
     @@artists.each do |artist|
-      if artist_count[genre]
+      if artist_count[artist]
         artist_count += 1
       else 
         artist_count[genre] = 1
